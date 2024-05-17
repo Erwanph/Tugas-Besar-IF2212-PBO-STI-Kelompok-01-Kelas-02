80b262bf7388fe48305cb25fde6bec32e7f1b358
hodakova <ahmadhabibiem@gmail.com>
2024-05-17 19:14:05 +0700

    initial grid

---
+++ src/main/Grids/DeckTanaman.java
@@ -0,0 +1,9 @@
+package main.Grids;
+import main.Grid;
+
+public class DeckTanaman implements Grid {
+    static final int maxSlot = 10;
+    public DeckTanaman() {}
+    public void addPlant() {}
+    public void removePlant() {}
+}
