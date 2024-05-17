cf60969f32383d6c4f641e14d63b46ff870f9c1f
Filbert Fuvian <ffuvian@gmail.com>
2024-05-17 18:36:27 +0700

    Zombie

--- src/main/Zombie.java
+++ src/main/Zombie.java
@@ -1,5 +1,22 @@
 package main;
 
-public class Zombie {
-    
+public abstract class Zombie extends GameObject {
+    private int speed;
+    private int attackSpeed;
+    private int attackDamage;
+    private boolean isSlowed;
+    private boolean isAquatic;
+
+
+    public Zombie(String name, int health, int speed, int attackSpeed, int attackDamage, boolean isSlowed, boolean isAquatic){
+        super(name, health);
+        this.speed = speed;
+        this.attackSpeed = attackSpeed;
+        this.attackDamage = attackDamage;
+        this.isSlowed = isSlowed;
+        this.isAquatic = isAquatic;
+    }
+    public int getSpeed(){
+
+    }
 }
