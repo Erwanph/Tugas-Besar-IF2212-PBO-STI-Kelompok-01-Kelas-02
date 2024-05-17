e0422a183eee4fa1edf4cec7b6c435d857021bbc
Filbert Fuvian <ffuvian@gmail.com>
2024-05-17 18:44:37 +0700

    Update Zombie.java

--- src/main/Zombie.java
+++ src/main/Zombie.java
@@ -7,7 +7,6 @@ public abstract class Zombie extends GameObject {
     private boolean isSlowed;
     private boolean isAquatic;
 
-
     public Zombie(String name, int health, int speed, int attackSpeed, int attackDamage, boolean isSlowed, boolean isAquatic){
         super(name, health);
         this.speed = speed;
@@ -17,6 +16,38 @@ public abstract class Zombie extends GameObject {
         this.isAquatic = isAquatic;
     }
     public int getSpeed(){
+        return speed;
+    }
 
+    public void setSpeed(int speed){
+        this.speed = speed;
+    }
+
+    public int getAttackSpeed(){ 
+        return attackSpeed;
+    }
+
+    public void setAttackSpeed(int attackSpeed){
+        this.attackSpeed = attackSpeed;
+    }
+
+    public int getAttackDamage(){ 
+        return attackDamage;
+    }
+
+    public void setAttackDamage(int attackDamage){
+        this.attackDamage = attackDamage;
+    }
+
+    public boolean getIsSlowed(){
+        return isSlowed;
+    }
+
+    public void setIsSlowed(boolean isSlowed){
+        this.isSlowed = isSlowed;
+    }
+
+    public boolean getIsAquatic(){
+        return isAquatic;
     }
 }
